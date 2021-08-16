e94be052b3cef2e27bf6c90af28f7d29fad05e14
Kimminjong94 <79985912+Kimminjong94@users.noreply.github.com>
2021-08-16 16:36:36 +0900

    Add files via upload
    Swift toy project based on online lecture

---
+++ MyDream/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MyDream
+//
+//  Created by 김민종 on 2021/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+}
+
